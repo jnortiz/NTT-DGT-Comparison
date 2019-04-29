679947c65a35e959d8a842e814f83c939f4d6e42
Jheyne N. Ortiz <jheyne.ortiz@students.ic.unicamp.br>
2019-04-29 14:03:05 -0300

    Reduction mod 2**64-2**21+1.

--- src/dgt/gauss.c
+++ src/dgt/gauss.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include <time.h>
 
 uint64_t p = 0xFFFFFFFF00000001; 
-// 18 446 744 069 414 584 321
-// 18 446 744 065 119 617 026
+unsigned __int128 max_128 = ((unsigned __int128)0xFFFFFFFFFFFFFFFFu << 64) + 0xFFFFFFFFFFFFFFFFu;
+
 struct gauss {
 	uint64_t re;
 	uint64_t img;
@@ -18,7 +19,6 @@ void Gauss(gauss_t *pointer, uint64_t re, uint64_t img) {
 	pointer->img = img;
 }
 
-/* Print a Gaussian integer */
 void print_gauss_t(const gauss_t x) {
 	printf("%" PRId64, x.re);
 	if(x.img >= 0)
@@ -26,67 +26,66 @@ void print_gauss_t(const gauss_t x) {
 	printf("%" PRId64 "i\n", x.img);
 }
 
-/* Compute the conjugate of a Gaussian integer */
 gauss_t conjugate(const gauss_t x){
 	gauss_t conj_x;
 	Gauss(&conj_x, x.re, -x.img);
 	return conj_x;
 }
 
-/* Return true if x is equal to y */
-/*int is_equal(const gauss_t x, const gauss_t y) {
-	return (x.re == y.re && x.img == y.img);
-}*/
-
 unsigned less_than(const uint64_t x, const uint64_t y) {
 	uint64_t less = (x-y);
 	return (less >> sizeof(uint64_t)*8-1);
 }
 
-int64_t select_int64_t(uint64_t x, uint64_t y, uint64_t bit) {
+uint64_t select_uint64_t(uint64_t x, uint64_t y, uint64_t bit) {
 	uint64_t mask = -bit;
-	int64_t output = mask & (x ^ y);
+	uint64_t output = mask & (x ^ y);
 	return (output ^ x);
 }
 
-int64_t is_equal(uint64_t x, uint64_t y) {
-	uint64_t zero = (x-y);
-	return (1 ^ ((zero | -zero) >> 63) & 1);
+uint64_t mod_uint64_t(const uint64_t a) {	
+	uint64_t output;
+	output = select_uint64_t(output, output-p, !less_than(output,p));
+	return output;
 }
 
-/* Compute a mod b in Z_p */
-uint64_t mod(unsigned __int128 a) {	
+uint64_t mod_uint128(const unsigned __int128 a) {	
+	uint64_t gap, w0, w1, w2, w3, W1, W2, sum, output;
+	int8_t negative_overflow, greater_than_p, double_overflow;
 
-	uint64_t w0, w1, w2, w3, c;
+	gap = (uint64_t)(18446744073709551615u)-p;
 
-	w0 = a & 0xFFFFFFFF;
-	//printf("%" PRIu64 "\n", w0);
-	w1 = ((uint64_t)(a >> 32)) & 0xFFFFFFFF;
-	//printf("%" PRIu64 "\n", w1);
-	w2 = ((uint64_t)(a >> 64)) & 0xFFFFFFFF;
-	//printf("%" PRIu64 "\n", w2);
-	w3 = (uint64_t)(a >> 96);
-	//printf("%" PRIu64 "\n", w3);
-	c = (w2+w1) << 32;
-	//printf("%" PRIu64 "\n", c);
+	w0 = (uint32_t) a;
+	w1 = (uint32_t)(a >> 32);
+	w2 = (uint32_t)(a >> 64);
+	w3 = (uint32_t)(a >> 96);
 
-	c = c-w3-w2+w0;
-	printf("Before select %" PRIu64 " | ", c);
-	c = select_int64_t(c,c-p,less_than(p, c)||is_equal(p,c));
-	printf("After select %" PRIu64 " ", c);
+	W1 = w1 << 32;
+	W2 = w2 << 32;
+	sum = W1+W2;
 
-	return c;
+	output = sum-w3-w2+w0;
+
+	negative_overflow = (w2+w3 > sum+w0) && !(less_than(W1,W2)||less_than(sum,w0));
+	greater_than_p = (output >= p);
+	double_overflow = (less_than(W1,W2) || less_than(sum,w0)) && (sum+w0 > w2+w3);
+
+	output = (p-output)*negative_overflow
+			+ (output-p)*(!negative_overflow && greater_than_p)
+			+ (output+gap)*(!negative_overflow && !greater_than_p && double_overflow)
+		   	+ output*(!(negative_overflow || greater_than_p || double_overflow));
+
+	return output;
 }
 
-/*
 void add(gauss_t *z, const gauss_t x, const gauss_t y) {
-	z->re = mod(x.re + y.re);
-	z->img = mod(x.img + y.img);
+	z->re = mod_uint64_t(x.re + y.re);
+	z->img = mod_uint64_t(x.img + y.img);
 }
 
 void sub(gauss_t *z, const gauss_t x, const gauss_t y) {
-	z->re = mod(x.re - y.re);
-	z->img = mod(x.img - y.img);
+	z->re = mod_uint64_t(x.re - y.re);
+	z->img = mod_uint64_t(x.img - y.img);
 }
 
 void mul(gauss_t *z, const gauss_t x, const gauss_t y) {
@@ -96,25 +95,21 @@ void mul(gauss_t *z, const gauss_t x, const gauss_t y) {
 	s2 = x.img * y.img;
 	s3 = (x.re + x.img)*(y.re + y.img);
 
-	z->re = mod(s1 - s2);
-	z->img = mod(s3 - s1 - s2);
-}*/
+	z->re = mod_uint128(s1 - s2);
+	z->img = mod_uint128(s3 - s1 - s2);
+}
+
+unsigned __int128 get_uint128_word() {
+	__int128 a;
+	a = rand() % max_128;
+	return a;
+}
 
 int main(int argc, char** argv[]) {
-	//gauss_t x, y, z;
-	//Gauss(&x, 1, -2);
-	//Gauss(&y, 1, 2);
-
-	//sub(&z, x, y);
-	//print_gauss_t(z);
-	unsigned __int128 a;
+	srand(time(NULL));
 	int i;
-	for(i = 0; i < 32; i++) {
-		a = (p << i);
-		printf("Reducing: p << %d | ", i);
-		mod(a);
-		printf("\n");
+	for(i = 0; i < 128; i++) {
+		printf("%" PRIu64 "\n", mod_uint128(get_uint128_word()));
 	}
-
 	return 0;
 }
