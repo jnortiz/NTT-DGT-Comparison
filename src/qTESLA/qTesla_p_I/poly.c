8ae7e2d45db1f243a713c488ea0a0c57453b70a1
Jheyne N. Ortiz <jheyne.ortiz@ic.unicamp.br>
2019-10-30 16:43:55 -0300

    DGT + NewHope with source code of August 16, 2019.

---
+++ src/qTESLA/qTesla_p_I/poly.c
@@ -0,0 +1,371 @@
+/*************************************************************************************
+* qTESLA: an efficient post-quantum signature scheme based on the R-LWE problem
+*
+* Abstract: DGT, modular reduction and polynomial functions
+**************************************************************************************/
+
+#include "poly.h"
+#include "params_dgt.h"
+#include "sha3/fips202.h"
+#include "api.h"
+
+extern poly zeta;
+extern poly zetainv;
+
+
+void poly_uniform(poly_k a, const unsigned char *seed)         
+{ // Generation of polynomials "a_i"
+  unsigned int pos=0, i=0, nbytes = (PARAM_Q_LOG+7)/8;
+  unsigned int nblocks=PARAM_GEN_A;
+  uint32_t val1, val2, val3, val4, mask = (uint32_t)(1<<PARAM_Q_LOG)-1;
+  unsigned char buf[SHAKE128_RATE*PARAM_GEN_A];
+  uint16_t dmsp=0;
+
+  cshake128_simple(buf, SHAKE128_RATE*PARAM_GEN_A, dmsp++, seed, CRYPTO_RANDOMBYTES);    
+     
+  while (i < PARAM_K*PARAM_N) {   
+    if (pos > SHAKE128_RATE*nblocks - 4*nbytes) {
+      nblocks = 1;
+      cshake128_simple(buf, SHAKE128_RATE*nblocks, dmsp++, seed, CRYPTO_RANDOMBYTES);    
+      pos = 0;
+    } 
+    val1  = (*(uint32_t*)(buf+pos)) & mask;
+    pos += nbytes;
+    val2  = (*(uint32_t*)(buf+pos)) & mask;
+    pos += nbytes;
+    val3  = (*(uint32_t*)(buf+pos)) & mask;
+    pos += nbytes;
+    val4  = (*(uint32_t*)(buf+pos)) & mask;
+    pos += nbytes;
+    if (val1 < PARAM_Q && i < PARAM_K*PARAM_N)
+      a[i++] = reduce((int64_t)val1*PARAM_R2_INVN);
+    if (val2 < PARAM_Q && i < PARAM_K*PARAM_N)
+      a[i++] = reduce((int64_t)val2*PARAM_R2_INVN);
+    if (val3 < PARAM_Q && i < PARAM_K*PARAM_N)
+      a[i++] = reduce((int64_t)val3*PARAM_R2_INVN);
+    if (val4 < PARAM_Q && i < PARAM_K*PARAM_N)
+      a[i++] = reduce((int64_t)val4*PARAM_R2_INVN);
+  }
+}
+
+
+int32_t reduce(int64_t a)
+{ // Montgomery reduction
+  int64_t u;
+
+  u = (a*PARAM_QINV) & 0xFFFFFFFF;
+  u *= PARAM_Q;
+  a += u;
+  return (int32_t)(a>>32);
+}
+
+
+int64_t barr_reduce64(int64_t a)
+{ // Barrett reduction
+  int64_t u = (a*PARAM_BARR_MULT)>>PARAM_BARR_DIV;
+  return a - u*PARAM_Q;
+}
+
+
+sdigit_t barr_reduce(sdigit_t a)
+{ // Barrett reduction
+  digit_t u = ((int64_t)a*PARAM_BARR_MULT)>>PARAM_BARR_DIV;
+  return a - (digit_t)u*PARAM_Q;
+}
+
+
+void dgt(poly x)
+{
+  int i, index, j, m, window;
+  int32_t a, sub_re, sub_img;
+
+  m = PARAM_K2;
+  window = 1;
+  for(; m >= 4; m >>= 1) {
+    index = 0;
+    for(j = 0; j < m; j += 2) {
+      a = _gj[index];
+
+      for(i = j; i < PARAM_N; i += (m << 1)) {
+      sub_re = (x[i] + (2LL*PARAM_Q - x[i+m]));
+      sub_img = (x[i+1] + (2LL*PARAM_Q -x[i+m+1]));
+      
+      x[i] = (x[i] + x[i+m]);
+      x[i+1] = (x[i+1] + x[i+m+1]);
+      
+      x[i+m] = reduce((int64_t)a * sub_re);
+      x[i+m+1] = reduce((int64_t)a * sub_img);        
+      }
+      index += window;
+    }
+    window <<= 1;
+
+    m >>= 1;
+
+    index = 0;
+    for(j = 0; j < m; j += 2) {
+      a = _gj[index];
+
+      for(i = j; i < PARAM_N; i += (m << 1)) {
+      sub_re = (x[i] + (2LL*PARAM_Q - x[i+m]));
+      sub_img = (x[i+1] + (2LL*PARAM_Q -x[i+m+1]));
+      
+      x[i] = barr_reduce64(x[i] + x[i+m]);
+      x[i+1] = barr_reduce64(x[i+1] + x[i+m+1]);
+      
+      x[i+m] = reduce((int64_t)a * sub_re);
+      x[i+m+1] = reduce((int64_t)a * sub_img);        
+      }
+      index += window;
+    }
+    window <<= 1;
+  }       
+
+  index = 0;
+  for(j = 0; j < m; j += 2) {
+    a = _gj[index];
+
+    for(i = j; i < PARAM_N; i += (m << 1)) {
+    sub_re = (x[i] + (2LL*PARAM_Q - x[i+m]));
+    sub_img = (x[i+1] + (2LL*PARAM_Q -x[i+m+1]));
+    
+    x[i] = (x[i] + x[i+m]);
+    x[i+1] = (x[i+1] + x[i+m+1]);
+    
+    x[i+m] = reduce((int64_t)a * sub_re);
+    x[i+m+1] = reduce((int64_t)a * sub_img);        
+    }
+    index += window;
+  }
+}
+
+
+void idgt(poly x)
+{
+
+int i, index, j, m, window;
+int32_t a, mul_re, mul_img;
+
+window = (PARAM_K2 >> 1);
+  for(m = 2; m <= 256; m <<= 1) {
+    index = 0;
+    for(j = 0; j < m; j += 2) {
+      a = _invgj[index];
+
+      for(i = j; i < PARAM_N; i += (m << 1)) {
+        mul_re = reduce((int64_t)x[i+m] * a);
+        mul_img = reduce((int64_t)x[i+m+1] * a);
+
+        x[i+m] = (x[i] + (2LL*PARAM_Q - mul_re));
+        x[i+m+1] = (x[i+1] + (2LL*PARAM_Q - mul_img));
+
+        x[i] = (x[i] + mul_re);
+        x[i+1] = (x[i+1] + mul_img);        
+        }
+        index += window;
+    }
+    window >>= 1;
+
+    m <<= 1; 
+
+    index = 0;
+    for(j = 0; j < m; j += 2) {
+      a = _invgj[index];
+
+      for(i = j; i < PARAM_N; i += (m << 1)) {
+        mul_re = reduce((int64_t)x[i+m] * a);
+        mul_img = reduce((int64_t)x[i+m+1] * a);
+
+        x[i+m] = barr_reduce64(x[i] + (2LL*PARAM_Q - mul_re));
+        x[i+m+1] = barr_reduce64(x[i+1] + (2LL*PARAM_Q - mul_img));
+
+        x[i] = barr_reduce64(x[i] + mul_re);
+        x[i+1] = barr_reduce64(x[i+1] + mul_img);        
+      }
+      index += window;
+    }
+    window >>= 1;
+  }
+
+  index = 0;
+  for(j = 0; j < m; j += 2) {
+    a = _invgj[index];
+
+    for(i = j; i < PARAM_N; i += (m << 1)) {
+      mul_re = reduce((int64_t)x[i+m] * a);
+      mul_img = reduce((int64_t)x[i+m+1] * a);
+
+      x[i+m] = (x[i] + (2LL*PARAM_Q - mul_re));
+      x[i+m+1] = (x[i+1] + (2LL*PARAM_Q - mul_img));
+
+      x[i] = (x[i] + mul_re);
+      x[i+1] = (x[i+1] + mul_img);        
+    }
+    index += window;
+  }
+
+}
+
+
+static void poly_pointwise(poly result, const poly x, const poly y)
+{ // Pointwise polynomial multiplication result = x.y
+
+  for (int i=0; i<PARAM_N; i++)
+    result[i] = reduce((int64_t)x[i]*y[i]);
+}
+
+
+void poly_dgt(poly x_dgt, const poly x)
+{
+
+  int i, j;
+
+  for(i = 0, j = 0; i < PARAM_N && j < PARAM_K2; i+=2, j++) {             
+      x_dgt[i] = (reduce(
+        (int64_t)x[j] * _nthroots[i] - 
+        (int64_t)x[PARAM_K2+j] * _nthroots[i+1]
+      ));
+      
+      x_dgt[i+1] = (reduce(
+        (int64_t)x[j] * _nthroots[i+1] + 
+        (int64_t)x[PARAM_K2+j] * _nthroots[i]
+      ));
+  } 
+
+  dgt(x_dgt);
+}
+
+
+void poly_mul(poly _output, const poly _poly_a, const poly _poly_b)
+{ /* It is assumed that both signals are already in the DGT domain. 
+     The DGT counterpart of poly_b was computed in sign.c. */
+  
+  poly _mul;
+  int i, j;
+
+  /* Calculating the point-wise multiplication of input signals */
+  for(i = 0, j = 0; i < PARAM_N && j < PARAM_K2; i+=2, j++) {             
+    _mul[i] = (reduce(
+      (int64_t)_poly_a[j] * _poly_b[i] -
+      (int64_t)_poly_a[j+PARAM_K2] * _poly_b[i+1]
+    ));
+
+    _mul[i+1] = (reduce(
+      (int64_t)_poly_a[j] * _poly_b[i+1] + 
+      (int64_t)_poly_a[j+PARAM_K2] * _poly_b[i]
+    ));
+  }
+
+  /* Recovering the multiplication result in Z[x]/<x^n+1> */
+  idgt(_mul);
+
+  /* Removing the twisting factors and writing the result from the Gaussian integer to the polynomial form */
+  for(i = 0, j = 0; i < PARAM_N && j < PARAM_K2; i+=2, j++) {
+      _output[j] = barr_reduce64(
+        reduce((int64_t)_mul[i] * _invnthroots[i]) +
+        (2LL*PARAM_Q - reduce((int64_t)_mul[i+1] * _invnthroots[i+1]))
+      );
+
+      _output[j+PARAM_K2] = barr_reduce64(
+        reduce((int64_t)_mul[i] * _invnthroots[i+1]) + 
+        reduce((int64_t)_mul[i+1] * _invnthroots[i])
+      );
+  }
+}
+
+
+void poly_add(poly result, const poly x, const poly y)
+{ // Polynomial addition result = x+y
+
+    for(int i=0; i<PARAM_N; i++)
+      result[i] = x[i] + y[i];
+}
+
+
+void poly_add_correct(poly result, const poly x, const poly y)
+{ // Polynomial addition result = x+y with correction
+
+    for (int i=0; i<PARAM_N; i++) {
+      result[i] = x[i] + y[i];
+      result[i] += (result[i] >> (RADIX32-1)) & PARAM_Q;    // If result[i] < 0 then add q
+      result[i] -= PARAM_Q;
+      result[i] += (result[i] >> (RADIX32-1)) & PARAM_Q;    // If result[i] >= q then subtract q
+    }
+}
+
+
+void poly_sub(poly result, const poly x, const poly y)
+{ // Polynomial subtraction result = x-y
+
+    for (int i=0; i<PARAM_N; i++)
+      result[i] = x[i] - y[i];
+}    
+
+
+void poly_sub_reduce(poly result, const poly x, const poly y)
+{ // Polynomial subtraction result = x-y
+
+    for (int i=0; i<PARAM_N; i++)
+      result[i] = (int32_t)barr_reduce(x[i] - y[i]);
+}
+
+
+/********************************************************************************************
+* Name:        sparse_mul8
+* Description: performs sparse polynomial multiplication
+* Parameters:  inputs:
+*              - const unsigned char* s: part of the secret key
+*              - const uint32_t pos_list[PARAM_H]: list of indices of nonzero elements in c
+*              - const int16_t sign_list[PARAM_H]: list of signs of nonzero elements in c
+*              outputs:
+*              - poly prod: product of 2 polynomials
+*
+* Note: pos_list[] and sign_list[] contain public information since c is public
+*********************************************************************************************/
+void sparse_mul8(poly prod, const unsigned char *s, const uint32_t pos_list[PARAM_H], const int16_t sign_list[PARAM_H])
+{
+  int i, j, pos;
+  int8_t *t = (int8_t*)s;
+
+  for (i=0; i<PARAM_N; i++)
+    prod[i] = 0;
+
+  for (i=0; i<PARAM_H; i++) {
+    pos = pos_list[i];
+    for (j=0; j<pos; j++) {
+        prod[j] = prod[j] - sign_list[i]*t[j+PARAM_N-pos];
+    }
+    for (j=pos; j<PARAM_N; j++) {
+        prod[j] = prod[j] + sign_list[i]*t[j-pos];
+    }
+  }
+}
+
+
+/********************************************************************************************
+* Name:        sparse_mul32
+* Description: performs sparse polynomial multiplication 
+* Parameters:  inputs:
+*              - const int32_t* pk: part of the public key
+*              - const uint32_t pos_list[PARAM_H]: list of indices of nonzero elements in c
+*              - const int16_t sign_list[PARAM_H]: list of signs of nonzero elements in c
+*              outputs:
+*              - poly prod: product of 2 polynomials
+*********************************************************************************************/
+void sparse_mul32(poly prod, const int32_t *pk, const uint32_t pos_list[PARAM_H], const int16_t sign_list[PARAM_H])
+{
+  int i, j, pos;
+  int64_t temp[PARAM_N] = {0};
+  
+  for (i=0; i<PARAM_H; i++) {
+    pos = pos_list[i];
+    for (j=0; j<pos; j++) {
+        temp[j] = temp[j] - sign_list[i]*pk[j+PARAM_N-pos];
+    }
+    for (j=pos; j<PARAM_N; j++) {
+        temp[j] = temp[j] + sign_list[i]*pk[j-pos];
+    }
+  }
+  for (i=0; i<PARAM_N; i++)
+    prod[i] = (int32_t)barr_reduce64(temp[i]);
+}
